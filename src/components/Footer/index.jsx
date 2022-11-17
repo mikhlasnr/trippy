fa0af73d2a8fc6ba1c6ec145df852414a991aabe
mikhlasnr <minrnullpo@gmail.com>
2022-11-17 18:13:19 +0700

    create footer and nav scroll change bg

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
-
+import React from "react"
+import "./Footer.scss"
+import LogoFooter from "../../assets/images/logo-footer.svg"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer id="footer-main">
+      <div className="container footer-main__content ">
+        <div className="footer-main__content__left">
+          <img src={LogoFooter} />
+          <h2>Trip to be happy</h2>
+          <p>
+            Membantu market trip dan travel lebih aksesibel, dan juga
+            berkontribusi mempromosikan pariwisata di Indonesia dengan
+            pengalaman trip yang menyenangkan
+          </p>
+        </div>
+        <div className="footer-main__content__right">
+          <div className="footer-main__content__right__item">
+            <h2>Sitemap</h2>
+            <a href="#">Home</a>
+            <a href="#">Benefit</a>
+            <a href="#">Trip</a>
+            <a href="#">Kontak</a>
+          </div>
+          <div className="footer-main__content__right__item">
+            <h2>Kontak</h2>
+            <p>Telepon: +62 85157933252</p>
+            <p>Instagram: trippyofficial</p>
+            <p> Jl Letkol Istiqlah 93 68422</p>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,7 +1,19 @@
-import React from "react"
+import React, {useEffect} from "react"
 import "./Nav.scss"
 import { ReactComponent as Logo } from "../../assets/images/logo-nav.svg"
 const Nav = () => {
+  useEffect(() => {
+    const getNavbar = document.getElementById("navbar-main");
+    window.addEventListener('scroll', function () {
+      const scrollFromTop = window.pageYOffset
+      if(scrollFromTop > 0){
+        getNavbar.classList.add("navbar-main--scroll")
+      }else{
+        getNavbar.classList.remove("navbar-main--scroll")
+      }
+    })
+  }, [])
+  
   return (
     <nav id="navbar-main">
       <div className="container">
